eacc6101aa18c4e3bc5c19ea2eb65654b3ef6946
Ben <feba23@live.com>
2017-12-12 12:25:56 -0600

    Removed the notification increment gun

--- Group Connection/JoinOrCreateViewContoller.swift
+++ Group Connection/JoinOrCreateViewContoller.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class JoinOrCreateViewController: UIViewController {
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
     
     
     
