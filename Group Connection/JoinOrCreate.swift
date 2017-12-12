a2522a86dc9707cfa4245882d95759cc501ec164
Ben <feba23@live.com>
2017-12-12 11:16:47 -0600

    Stuff committed

---
+++ Group Connection/JoinOrCreate.swift
@@ -0,0 +1,11 @@
+//
+//  JoinOrCreate.swift
+//  Group Connection
+//
+//  Created by BARCASKEY, BENJAMIN on 12/12/17.
+//  Copyright © 2017 District196. All rights reserved.
+//
+
+import Foundation
+
+
