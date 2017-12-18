efb7064ecb67be4efbf2947926ae23ca910ee23c
Ben <feba23@live.com>
2017-12-18 12:21:59 -0600

    Add files via upload

---
+++ Class Structures/Globals.swift
@@ -0,0 +1,22 @@
+//
+//  Globals.swift
+//  Group Connection
+//
+//  Created by Daniel e. Naranjo Sampson on 12/13/17.
+//  Copyright © 2017 District196. All rights reserved.
+//
+/*  Complete and total stub; fields in here need to be alterable from other classes or straight up in other classes.
+*/
+import Foundation
+
+class Globals {
+    var initialized, inEvent: Bool
+    var isMentor: Bool
+    
+    init() {
+        initialized = false
+        inEvent = false
+        isMentor = false
+    }
+    
+}
