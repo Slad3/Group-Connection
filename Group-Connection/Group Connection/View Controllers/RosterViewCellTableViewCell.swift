e124b7029850eed333e1fcabe0de2086087a28dc
Micknick2 <ndburright@icloud.com>
2018-02-08 11:36:33 -0600

    Create new file.
    asdf

---
+++ Group-Connection/Group Connection/View Controllers/RosterViewCellTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  RosterViewCellTableViewCell.swift
+//  Group Connection
+//
+//  Created by BURRIGHT, NICHOLAS on 1/31/18.
+//  Copyright © 2018 District196. All rights reserved.
+//
+
+import UIKit
+
+class RosterViewCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullName: UILabel!
+   
+  
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+        
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        
+        
+        
+        
+        
+        
+        
+    }
+
+}
