60aca3c97143a233e00c37af9ad471170438c04b
Micknick2 <ndburright@icloud.com>
2018-02-08 11:47:12 -0600

    sg

--- Group-Connection/Group Connection/Class Structures/Person.swift
+++ Group-Connection/Group Connection/Class Structures/Person.swift
@@ -68,7 +68,7 @@ class Person {
             return VoodooMagic(people: people)
         }
         
-        func makePeople() -> [Person] {
+        func sex() -> [Person] {
             var temp: [Person] = []
             for dude in people {
                 temp.append(dude.toPerson())
@@ -159,7 +159,7 @@ class Person {
             jsonData = try Data(contentsOf: ArchiveURL)
             let roster = try JSONDecoder().decode(Roster.self, from: jsonData)
             print(String(data: jsonData, encoding: .utf8)!)
-            return roster.makePeople()
+            return roster.sex()
         }
         catch {
             print("decoding failed")
