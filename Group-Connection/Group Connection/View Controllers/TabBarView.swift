3e22cc08ddcbeaf2be6b6844f2044f4643aaf0a7
Ben <feba23@live.com>
2018-02-15 12:14:22 -0600

    Did a bunch of stuff with multipeer stuff working

---
+++ Group-Connection/Group Connection/View Controllers/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  Group Connection
+//
+//  Created by BARCASKEY, BENJAMIN on 2/15/18.
+//  Copyright © 2018 District196. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabBarView: UITabBar {
+    
+    
+    
+    
+    
+}
