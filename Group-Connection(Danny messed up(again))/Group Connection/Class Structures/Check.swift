c6fd60fea0d88c34555cc1981d98f587b40fe793
Michael Kelm <34281823+thebubbakelm@users.noreply.github.com>
2018-04-02 11:41:08 -0500

    hg

---
+++ Group-Connection(Danny messed up(again))/Group Connection/Class Structures/Check.swift
@@ -0,0 +1,53 @@
+//
+//  Check.swift
+//  Group Connection
+//
+//  Created by Daniel e. Naranjo Sampson on 1/5/18.
+//  Copyright © 2018 District196. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Check: NSObject, MKAnnotation {
+    var sender: Person
+    var coordinate: CLLocationCoordinate2D //sender's location. named coordinate because of interface stuff
+    var title: String? //also interface stuff
+    var subtitle: String? //interface stuff
+    var senderDescription: String
+    var timeSent: Date
+    
+    var hasBeenSent: Bool
+    
+    init(sender: Person, place: CLLocation, description: String) {
+        let location = place.coordinate
+        
+        self.sender = sender
+        self.coordinate = location
+        self.senderDescription = description
+        self.timeSent = Date()
+        self.title = sender.firstName
+        self.subtitle = senderDescription
+        self.hasBeenSent = false
+    }
+    
+    func sendThisCheck(_ person: Person) {
+        //fill in once we've gotten the connectivity stuff figured out
+        //has to somehow trigger receiveCheck() on the receiver's phone
+        do {
+            let temp = try JSONEncoder().encode("receiveCheck")
+            try Globals.globals.manager.session.send(temp, toPeers: [Globals.globals.manager.peerid], with: .reliable)
+            self.hasBeenSent = true
+        }
+        catch {
+            print("sending check failed")
+        }
+    }
+    
+    static func receiveCheck(check: Check) {
+        Globals.globals.user?.hasCheckIn = true
+        Globals.globals.user?.addCheck(check: check)
+        print("Check \(check.sender.firstName) received")
+    }
+}
+
---
+++ Group-Connection(Danny messed up(again))/Group Connection/View Controllers/Tab Views/Sub.swift
@@ -0,0 +1,14 @@
+//
+//  Sub.swift
+//  Group Connection
+//
+//  Created by NARANJO, DANIEL on 3/16/18.
+//  Copyright © 2018 District196. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Sub: UIViewController {
+    var isCheckInView: Bool! = false 
+}
