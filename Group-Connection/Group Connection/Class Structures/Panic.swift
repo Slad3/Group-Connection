493737383d8822c7999be982975c598915df1ebe
Slad3 <feba23@live.com>
2018-04-27 15:42:00 -0500

    Panic class for future use maybe

---
+++ Group-Connection/Group Connection/Class Structures/Panic.swift
@@ -0,0 +1,65 @@
+////
+////  Panic.swift
+////  Group Connection
+////
+////  Created by Ben on 4/27/18.
+////  Copyright © 2018 District196. All rights reserved.
+////
+//
+//import Foundation
+//import MultipeerConnectivity
+//import MapKit
+//
+//class Panic: NSObject, MKAnnotation{
+//    var sender: Person
+//    var coordinate: CLLocationCoordinate2D
+//    var timeSent: Date
+//
+//    struct penac: Codable{
+//        var sender: Person
+//        var coordinate: CLLocationCoordinate2D
+//        var timeSent: Date
+//        var latitude: Double
+//        var longitude: Double
+//        var timeSent: String
+//
+//        init(_ panic: Panic){
+//            let formatter = DateFormatter()
+//            formatter.dateStyle = .medium
+//            formatter.timeStyle = .medium
+//            formatter.locale = Locale(identifier: "en_US")
+//            timeSent = formatter.string(from: panic.timeSent)
+//            sender = Person.Persoon(panic.sender)
+//            latitude = panic.coordinate.latitude
+//            longitude = panic.coordinate.longitude
+//        }
+//
+//
+//
+//
+//
+//
+//    }
+//
+//
+//
+//    init(dude: Person, sentCoordinate: CLLocationCoordinate2D){
+//        sender = dude
+//        coordinate = sentCoordinate
+//        timeSent = Date()
+//
+//
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
+
